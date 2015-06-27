9c135752d13742c35893e434e7a63a41f30272eb
Newton Urbanetz <info@newtonurbanetz.com>
2015-06-27 14:58:02 +0200

    update ino

--- Matrix_Led-Serial-communication.ino
+++ Matrix_Led-Serial-communication.ino
@@ -5,7 +5,7 @@
 	 ***********************
 	
 	Fix: Newton Urbanetz
-			 ===============
+	     ===============
 
 	#fix PROGMEM prog_uchar CH[]  to PROGMEM const unsigned char CH[]
 	#fix //Put extracted character on Display "64" to "32" / m.writeSprite and m.setColumn "64" to "maxInUse*8"
